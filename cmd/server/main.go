f0b01e3614f793be6002d6fd23bcf164ba02adbd
enshxx <kuznecovilya12@gmail.com>
2024-08-29 15:05:19 +0300

    done iter1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"github.com/enshxx/GoMetricsHub/internal/server/handlers/update"
+	"github.com/enshxx/GoMetricsHub/internal/storage/memStorage"
+	"net/http"
+)
+
+func main() {
+	storage := memStorage.New()
+	mux := http.NewServeMux()
+	mux.Handle("/update/", http.StripPrefix(`/update/`, update.Handler(storage)))
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/enshxx/GoMetricsHub
+
+go 1.21.3
